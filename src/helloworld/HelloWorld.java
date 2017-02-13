3e5a99d1362a6c1d7a07049749b810ac38db4b65
Ladur3827 <Ladur3827@7KWN6Y1.Student.UGDSB.ED>
2017-02-13 13:01:54 -0500

    Second change

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -17,7 +17,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.print("Hello World!");
-        System.out.println("Hi");
+        System.out.println("Hello again");
     }
     
 }
